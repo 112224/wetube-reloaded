604a77a60c6c35b4a36f7ff95de00bef7f1dae77
112224 <didgns25@naver.com>
2021-06-26 22:26:06 +0900

    connect to mongoDB

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,18 +1,18 @@
 export const join = (req, res) => {
-    return res.send("Join");
-}
+  return res.send("Join");
+};
 export const edit = (req, res) => {
-    return res.send("edit");
-}
+  return res.send("edit");
+};
 export const deleteUser = (req, res) => {
-    return res.send("delete");
-}
-export const login = (req, res) =>{
-    return res.send("login");
-}
-export const logout = (req, res) =>{
-    return res.send("logout");
-}
+  return res.send("delete");
+};
+export const login = (req, res) => {
+  return res.send("login");
+};
+export const logout = (req, res) => {
+  return res.send("logout");
+};
 export const see = (req, res) => {
-    return res.send("see");
-}
+  return res.send("see");
+};
