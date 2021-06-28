ccad25748fdfa07a1538bdbe102d8e60c36f87b5
112224 <didgns25@naver.com>
2021-06-28 21:40:47 +0900

    watch the video

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -8,15 +8,23 @@ export const homepageVideos = async (req, res) => {
     return res.render("server Error", { error });
   }
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch", { pageTitle: `Watching ` });
+  try {
+    const video = await Video.findById(id);
+    console.log(video);
+    return res.render("watch", { pageTitle: video.title, video });
+  } catch {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  }
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("edit", { pageTitle: `Editing ` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title} `, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
