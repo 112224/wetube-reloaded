16ed54be3313490a75df25d17f61b751b9f80f24
112224 <didgns25@naver.com>
2021-07-19 16:26:54 +0900

    edit the profile

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,9 +1,10 @@
 export const localMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
+  console.log("req.session.user :>> ", req.session.user);
   next();
 };
-export const protecteMiddleware = (req, res, next) => {
+export const protectorMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return res.redirect("/");
   }
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,12 +7,12 @@ import {
   finishGithubLogin,
   postEdit,
 } from "../Controllers/userController";
-import { protecteMiddleware, publicOnlyMiddleware } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", protecteMiddleware, logout);
-userRouter.route("/edit").all(protecteMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
