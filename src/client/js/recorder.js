655b0332b3e117296ad277e9bf94b1961941f5fd
112224 <didgns25@naver.com>
2021-08-22 18:31:45 +0900

    webm to mp4 using ffmpeg wasm

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn");
 const downloadBtn = document.getElementById("downloadBtn");
 const preview = document.getElementById("preview");
@@ -53,7 +54,13 @@ const handleStart = () => {
 const handleRestart = () => {
   window.location.href = "/videos/upload";
 };
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "MyRecording.webm";
