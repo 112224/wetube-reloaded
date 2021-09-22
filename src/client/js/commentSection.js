2bd313995a7b47e0b0e9a315b58829b4ed49d49e
112224 <didgns25@naver.com>
2021-09-23 06:22:19 +0900

    bulid api router for comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,11 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: {
+      text: text,
+    },
+  });
 };
-btn.addEventListener("submit", handleSubmit);
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerView } from "../Controllers/videoController";
+import { registerView, createComment } from "../Controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
-
+apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
 export default apiRouter;
